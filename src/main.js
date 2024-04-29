4ec747ad4b3224198ae75eeaf29f3bd53b2dc4fe
Camila Mendez <mcmendezmanotas@gmail.com>
2024-04-28 23:08:21 -0500

    adicionando las vistas de la navegacion

--- src/main.js
+++ src/main.js
@@ -11,8 +11,8 @@ async function getTrendingMoviesPreview() {
   const {data} = await api('trending/movie/day');
 
   const movies = data.results;
+    trendingMoviesPreviewList.innerHTML = "";
   movies.forEach(movie => {
-    const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
     
     const movieContainer = document.createElement('div');
     movieContainer.classList.add('movie-container');
@@ -26,17 +26,15 @@ async function getTrendingMoviesPreview() {
     );
 
     movieContainer.appendChild(movieImg);
-    trendingPreviewMoviesContainer.appendChild(movieContainer);
+    trendingMoviesPreviewList.appendChild(movieContainer);
   });
 }
 
 async function getCategoriesPreview() {
     const {data} = await api('genre/movie/list');
-
     const categories = data.genres;
-    console.log({categories})
+    categoriesPreviewList.innerHTML = ""; 
     categories.forEach(category => {
-    const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
     
     const categoryContainer = document.createElement('div');
     categoryContainer.classList.add('category-container');
@@ -48,8 +46,6 @@ async function getCategoriesPreview() {
 
     categoryTitle.appendChild(categoryTitleText);    
     categoryContainer.appendChild(categoryTitle);
-    previewCategoriesContainer.appendChild(categoryContainer);
+    categoriesPreviewList.appendChild(categoryContainer);
   });
 }
-getTrendingMoviesPreview();
-getCategoriesPreview();
